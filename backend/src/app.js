e053b140c04630ed75976a9269326a86b3d17b6a
codes.anishshrestha@gmail.com <gafolizero>
2025-12-23 22:01:51 +0545

    feat: initialize backend foundation and global error handling

---
+++ backend/src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./middleware/error.middleware');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Note API is running' });
+});
+
+app.use((req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
+
