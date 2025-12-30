86749a7cd12feb52bd92db2af753915501c10f78
codes.anishshrestha@gmail.com <gafolizero>
2025-12-30 06:50:11 +0545

    style: update icon and title name

--- backend/test-logger.js
+++
@@ -1,8 +0,0 @@
-const logger = require('./src/utils/logger');
-
-console.log('Logger Test');
-
-logger.error('Test ERROR message', { test: true, level: 'error' });
-logger.warn('Test WARN message', { test: true, level: 'warn' });
-logger.info('Test INFO message', { test: true, level: 'info' });
-logger.debug('Test DEBUG message', { test: true, level: 'debug' });
